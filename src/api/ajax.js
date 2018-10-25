5e5994631bdbd750f3160fa84da4f7ecba5aef94
tgq5431 <690052300@qq.com>
2018-10-25 10:58:33 +0800

    initial commit

---
+++ src/api/ajax.js
@@ -0,0 +1,30 @@
+// ajax请求函数模块
+// 返回值：promise对象
+import axios from 'axios'
+export default function ajax(url,data={},type="GET") {
+  return new Promise(function (resolve, reject) {
+    let promise
+    if(type==='GET'){
+      let dataStr=''
+      Object.keys(data).forEach(key=>{
+        dataStr+=key+'='+data[key]+'&'
+      })
+      if (dataStr!==''){
+        dataStr=dataStr.substring(0,dataStr.lastIndexOf('&'))
+        url=url+'?'+dataStr
+      }
+      //发送get请求
+      promise=axios.get(url)
+    }else{
+      //发送post请求
+      promise=axios.post(url,data)
+    }
+    promise.then(function (response) {
+      resolve(response.data)
+    })
+      .catch(error=>{
+        reject(error)
+      })
+  })
+}
+
